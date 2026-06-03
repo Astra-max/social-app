b74c0edd68fe8ab3759bfbb76cffcdc81f91c020
Amonoff <ochukaamon@gmail.com>
2026-06-03 21:55:51 +0300

    fix: wrong import path

--- backend/pkg/middleware/auth_middleware.go
+++ backend/pkg/middleware/auth_middleware.go
@@ -3,7 +3,7 @@ package middleware
 import (
 	"context"
 	"net/http"
-	"social-network/backend/pkg/services"
+	"social-network/backend/internal/services" 
 )
 
 type contextKey string
@@ -13,7 +13,6 @@ const UserIDKey contextKey = "userID"
 func AuthMiddleware(sessionService *services.SessionService) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 			cookie, err := r.Cookie("session_id")
 			if err != nil {
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
