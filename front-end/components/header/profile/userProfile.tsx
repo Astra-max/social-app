22e605792211e66b9550d0f6dfabbcc83fb82e46
Astra-max <odongo724@gmail.com>
2026-06-03 12:52:39 +0300

    feat: contains user profile component image and profile details

---
+++ front-end/components/header/profile/userProfile.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import style from "@/styles/profile.module.css"
+
+export default function UserProfileImage() {
+    return (
+        <div>
+            <div className={style.imageCont}>
+                <Image className={style.userImage} src={"/maodongo.jpeg"} width={80} height={80} alt="user profile image" />
+            </div>
+        </div>
+    )
+}
