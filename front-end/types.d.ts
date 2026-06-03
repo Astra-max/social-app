609a65f8074d382f752acb6297d0e8febd01e9ba
Astra-max <odongo724@gmail.com>
2026-06-03 18:56:59 +0300

    feat: add an intersection between post data and userprofile data

--- front-end/types.d.ts
+++ front-end/types.d.ts
@@ -8,4 +8,23 @@ export interface ButtonData {
     text: string;
     onClick?: ()=> void;
     variants?: string;
+    className?: string;
+    style?: {};
+    type?: "button" | "submit" | "reset";
 }
+
+export interface PostData {
+    postId: number;
+    timePosted: string;
+    description: string;
+    postImage: string;
+}
+
+export interface UserProfile {
+    userId: number;
+    fullName: string;
+    status: "public" | "private";
+    userProfileImage: string;
+}
+
+export type UserPostData = PostData & UserProfile
