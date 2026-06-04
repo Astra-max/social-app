38316db6d71a6784bb8b5b1f4a5f98fb382e121f
maodongo <odongo724@gmail.com>
2026-06-04 21:42:13 +0300

    feat: added follow users right side bar ui

---
+++ front-end/components/sidebar/follow.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { suggestedFollows } from "@/dummy";
+import { ButtonData, FollowUsers } from "@/types";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import style from "@/styles/follow.module.css";
+import { btnFollowStyle } from "@/styles/style";
+
+export function RightSideBarUI() {
+  return <Follow />;
+}
+
+export function Follow() {
+  return (
+    <div className={style.userSFollowMain}>
+        <p className={style.people}>People you know</p>
+      {suggestedFollows.map((data) => (
+        <FollowUserUI key={data.userId} data={data} />
+      ))}
+    </div>
+  );
+}
+
+export function FollowUserUI({ data }: { data: FollowUsers }) {
+  const buttonData: ButtonData = {
+    text: "follow",
+    style: btnFollowStyle,
+  };
+
+  return (
+    <div className={style.userSFollowCont}>
+      <div className={style.displayFollowProf}>
+        <div className={style.followImage}>
+          <Image
+            src={data.userProfileImage}
+            priority
+            fill
+            alt={`${data.fullName} profile image`}
+          />
+        </div>
+        <div>
+          <p>{data.fullName}</p>
+          <p>{data.location}</p>
+        </div>
+      </div>
+      <Button data={buttonData} />
+    </div>
+  );
+}
