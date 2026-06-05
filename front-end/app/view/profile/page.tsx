9fff38cddd37eb2ffcaaae2c023e5d6739e67d2f
maodongo <waoremaxwel17@gmail.com>
2026-06-05 18:27:59 +0300

    feat: moved profile route to view/profile route

---
+++ front-end/app/view/profile/page.tsx
@@ -0,0 +1,11 @@
+import UserPersonalData from "@/components/header/profile/profile";
+import DefaultLayout from "@/components/layouts/defaultLayout";
+
+
+export default function Profile() {
+    return (
+        <DefaultLayout>
+            <UserPersonalData />
+        </DefaultLayout>
+    )
+}
