2e8d3a91f0e0b99009a8234b3fc2baf0f35bf6c6
maodongo <waoremaxwel17@gmail.com>
2026-06-05 19:24:31 +0300

    all

--- front-end/components/auth/login.tsx
+++ front-end/components/auth/login.tsx
@@ -7,27 +7,25 @@ import { insertIcon } from "@/styles/style";
 import { Lock, Mail, Unlock } from "lucide-react";
 import { useState } from "react";
 
-
 const btnStyle: ButtonData = {
-    text: "login now!!!",
-    style: { backgroundColor: "var(--primary-theme)", border: "0rem" },
-    type: "submit",
-  };
+  text: "login now!!!",
+  style: { backgroundColor: "var(--primary-theme)", border: "0rem" },
+  type: "submit",
+};
 
-  const authProviderLoginStyle: ButtonData = {
-    text: "Login with Google",
-    style: {
-      background: "transparent",
-      border: "0.1rem solid var(--primary-theme)",
-    },
-    type: "submit",
-  };
-
+const authProviderLoginStyle: ButtonData = {
+  text: "Login with Google",
+  style: {
+    background: "transparent",
+    border: "0.1rem solid var(--primary-theme)",
+  },
+  type: "submit",
+};
 
 export default function LogIn() {
-    const [ toggle, setToggle ] = useState<boolean>(false)
+  const [toggle, setToggle] = useState<boolean>(false);
 
-    const handleSetToggle = () => setToggle((prev) => !prev);
+  const handleSetToggle = () => setToggle((prev) => !prev);
 
   return (
     <form className={style.loginMain} action="">
@@ -38,33 +36,36 @@ export default function LogIn() {
         </div>
         <div className={style.userInputCont}>
           <p>Email Address</p>
-          <div style={{position: "relative"}}>
+          <div style={{ position: "relative" }}>
             <Mail size={20} style={insertIcon} />
             <input
-            className={`${style.userInput}`}
-            type="email"
-            name="emailAddr"
-            id=""
-            placeholder="someone@gmail.com"
-            required
-          />
+              className={`${style.userInput}`}
+              type="email"
+              name="emailAddr"
+              id=""
+              placeholder="someone@gmail.com"
+              required
+            />
           </div>
         </div>
 
         <div className={style.userInputCont}>
           <span>
             <p>Password</p>
-            <p style={{color: "var(--primary-theme)", cursor: "pointer"}}>Forgot password</p>
+            <p style={{ color: "var(--primary-theme)", cursor: "pointer" }}>
+              Forgot password
+            </p>
           </span>
           <div className={style.iconPosition}>
-            { !toggle ? 
-            <Lock size={24} style={insertIcon} onClick={handleSetToggle} /> :
-            <Unlock size={24} style={insertIcon} onClick={handleSetToggle} />
-            }
+            {!toggle ? (
+              <Lock size={24} style={insertIcon} onClick={handleSetToggle} />
+            ) : (
+              <Unlock size={24} style={insertIcon} onClick={handleSetToggle} />
+            )}
             <input
               className={`${style.userInput}`}
               name="password"
-              type={`${!toggle ? "password": "text"}`}
+              type={`${!toggle ? "password" : "text"}`}
               placeholder="*****************************"
               required
             />
@@ -81,8 +82,18 @@ export default function LogIn() {
           <Button data={authProviderLoginStyle} />
         </div>
         <div>
-          <p>Didn't have account 
-            <span style={{color: "var(--primary-theme)", cursor: "pointer", paddingLeft: "0.6rem"}}>register</span></p>
+          <p>
+            Didn't have account
+            <span
+              style={{
+                color: "var(--primary-theme)",
+                cursor: "pointer",
+                paddingLeft: "0.6rem",
+              }}
+            >
+              register
+            </span>
+          </p>
         </div>
       </div>
     </form>
