8e10b683f2833b6ebdf7265a2c729e1016447230
klucianob <kevobuyan@gmail.com>
2026-06-06 08:48:42 +0300

    feat(handlers): implement DeclineFollowRequest handler

--- backend/internal/handlers/follower_handler.go
+++ backend/internal/handlers/follower_handler.go
@@ -106,3 +106,42 @@ func (h *FollowerHandler) AcceptFollowRequest(w http.ResponseWriter, r *http.Req
 	})
 
 }
+
+func (h *FollowerHandler) DeclineFollowRequest(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	receiverID := middleware.GetUserID(r)
+
+	if receiverID == "" {
+
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	//step 3 get request_id from URL
+
+	requestID := r.PathValue("request_id")
+
+	if requestID == "" {
+
+		http.Error(w, "request_id is required", http.StatusBadRequest)
+		return
+	}
+
+	//step 4 Call the service
+	if err := h.followerService.DeclineFollowRequest(requestID, receiverID); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Step 5 — send success response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "follow request declined",
+	})
+
+}
