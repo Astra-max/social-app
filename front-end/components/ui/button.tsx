002cc556b952b8ca1f1e5a111961956a0f149fde
maodongo <waoremaxwel17@gmail.com>
2026-06-06 20:16:47 +0300

    feat: added a field icon to allow me pass icons to custom button ui

--- front-end/components/ui/button.tsx
+++ front-end/components/ui/button.tsx
@@ -1,30 +1,30 @@
-import { ButtonData } from "@/types"
+import { ButtonData } from "@/types";
 import { CSSProperties } from "react";
-import { useFormStatus } from "react-dom"
-
+import { useFormStatus } from "react-dom";
 
 const defaultBtnStyle: CSSProperties = {
-    padding: "0.8rem 3rem",
-    borderRadius: "0.4rem",
-    cursor: "pointer",
-    width: "100%",
-}
+  padding: "0.8rem 3rem",
+  borderRadius: "0.4rem",
+  cursor: "pointer",
+  width: "100%",
+};
 
+export function Button({ data }: { data: ButtonData }) {
+  const { pending } = useFormStatus();
 
-export function Button({data}: {data :ButtonData}) {
-    const { pending } = useFormStatus();
+  const btnStyle = { ...defaultBtnStyle, ...data.style };
 
-    const btnStyle  = { ...defaultBtnStyle, ...data.style}
+  const Icon = data?.icons;
 
-    return (
-        <button 
-        onClick={()=> data.onClick}
-        disabled={pending}
-        type={data.type}
-        style={btnStyle}
-        >
-            {pending ? "submitting...." : data.text}
-        </button>
-    )
-
+  return (
+    <button
+      onClick={() => data.onClick}
+      disabled={pending}
+      type={data.type}
+      style={btnStyle}
+    >
+        {Icon && <Icon size={30} />}
+        {pending ? "submitting...." : data.text}
+    </button>
+  );
 }
