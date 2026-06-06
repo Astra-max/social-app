a54fdc812377e08e64e2d9cf82524f34551ced03
maodongo <waoremaxwel17@gmail.com>
2026-06-06 17:38:34 +0300

    feat: creted all routes in view directory

---
+++ front-end/app/view/Following/page.tsx
@@ -0,0 +1,9 @@
+import DefaultLayout from "@/components/layouts/defaultLayout";
+
+export default function FollowingUI() {
+    return (
+        <DefaultLayout>
+            <div>Following page</div>
+        </DefaultLayout>
+    )
+}
