74b87683ca8b323f8f3b2e5ee362679ad6fcf72c
Astra-max <waoremaxwel17@gmail.com>
2026-06-16 10:42:06 +0300

    feat: reusable custom useSocket hook that will be used across several components

--- front-end/hooks/useSocket.ts
+++ front-end/hooks/useSocket.ts
@@ -1,16 +1,28 @@
 "use client"
 
+import { ChatMessages } from "@/types"
 import { useEffect, useRef, useState } from "react"
 
-export default function useSocket() {
+interface SocketType {
+    connected: boolean;
+    messages: ChatMessages[];
+    sendMessage: (message: ChatMessages) => void;
+}
+
+export default function useSocket(url: string): SocketType | void {
     const [connected, setConnected] = useState<boolean>(false)
+    const [messages, setMessages] = useState<ChatMessages[]>([])
     const socketRef = useRef<WebSocket | null>(null)
 
     const env = process.env.NODE_ENV
-    const devUrl = process.env.WS_DEV_URL
-    let url = ""
+    const devUrl = process.env.NEXT_PUBLIC_WS_DEV_URL
+
     if (env && env === "development" && devUrl) url = devUrl
 
+    if (url === "") {
+        console.log("socket url missing")
+        return;
+    }
     useEffect(()=> {
         const socket = new WebSocket(url);
         socketRef.current = socket;
@@ -23,5 +35,31 @@ export default function useSocket() {
             console.log("connection closed")
             setConnected(false)
         }
-    })
+        socket.onerror = (error: Event) => {
+            console.error(error)
+        }
+        socket.onmessage = (event: MessageEvent) => {
+            const message: ChatMessages = JSON.parse(event.data)
+            setMessages((prev)=> [...prev, message])
+        }
+        return ()=> {
+            socket.close()
+        }
+    },[url])
+
+    const sendMessage = (message: ChatMessages) => {
+        if (
+            socketRef.current &&
+            socketRef.current.readyState === WebSocket.OPEN 
+        ) {
+            socketRef.current.send(JSON.stringify(message))
+        }
+    }
+    
+    return {
+        connected,
+        messages,
+        sendMessage
+    }
+
 }
