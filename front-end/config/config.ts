91db0275587e1866f30864530bd44cec56e5c507
Astra-max <waoremaxwel17@gmail.com>
2026-06-16 17:12:05 +0300

    confg: pass websocket url and rest url dynamic depending on node environment

---
+++ front-end/config/config.ts
@@ -0,0 +1,54 @@
+interface Props {
+    urlType: string;
+    urlUsage: string;
+}
+
+interface Config {
+    sockectUrl?: string;
+    baseUrl?: string;
+}
+
+export default function loadEnvFile(urlProperties: Props): Config {
+    const config: Config = {}
+    const env = process.env.NODE_ENV
+
+    switch (urlProperties.urlType) {
+        case "socket-url":
+            switch (urlProperties.urlUsage) {
+                case "chat":
+                    if (
+                        env && env === "development"
+                    ) {
+                        const dev = process.env.NEXT_PUBLIC_WS_DEV_CHAT_URL ?? "ws://localhost:8080/ws/chat"
+                        config.sockectUrl = dev
+                    }
+                    break;
+                case "group":
+                     if (
+                        env && env === "development"
+                    ) {
+                        const dev = process.env.NEXT_PUBLIC_WS_DEV_GROUP_URL ?? "ws://localhost:8080/ws/group"
+                        config.sockectUrl = dev
+                    }
+                    break;
+                case "notifications":
+                    if (
+                        env && env === "development"
+                    ) {
+                        const dev = process.env.NEXT_PUBLIC_WS_DEV_NOTIFICATION_URL ?? "ws://localhost:8080/ws/notifications"
+                        config.sockectUrl = dev
+                    }
+                    break;          
+                default:
+                    console.log("provide url usage type")
+                    break;
+            }
+            break;
+        case "baseUrl":
+            break;
+        default:
+            console.log("missing url type [usage: socket-url] [usage: base-url]")
+            break;
+    }
+    return config
+}
