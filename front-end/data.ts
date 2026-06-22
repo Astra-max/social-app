c2f2a592972a5321078b020988d541a02dbb8499
Astra-max <waoremaxwel17@gmail.com>
2026-06-22 21:34:56 +0300

    feat: added an object field saved which will be interpreted as browser route

--- front-end/data.ts
+++ front-end/data.ts
@@ -1,4 +1,4 @@
-import { Home, Compass, Bell, MessageSquare, Users, Calendar, Settings, LogOut, User, UserPlus2Icon, UserMinusIcon } from 'lucide-react';
+import { Home, Compass, Bell, MessageSquare, Users, Calendar, Settings, LogOut, User, UserPlus2Icon, SaveIcon } from 'lucide-react';
 
 
 export const sidebarNav = [
@@ -8,6 +8,7 @@ export const sidebarNav = [
     {id: 4,icon: MessageSquare, text: "Messages"},
     {id: 5,icon: Users, text: "Groups"},
     {id: 6,icon: UserPlus2Icon, text: "Network"},
+    {id: 7,icon: SaveIcon, text: "Saved"},
     {id: 8,icon: Calendar, text: "Events"},
     {id: 9,icon: Settings, text: "Settings"},
     {id: 10, icon: User, text: "profile"},
