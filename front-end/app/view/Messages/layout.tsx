bea5f10f5cd5ff6d88450af0e14ef8869566788a
Astra-max <waoremaxwel17@gmail.com>
2026-06-23 19:31:54 +0300

    feat: wrapped arround authComponent to chat component to block unauthenticated users from chatted

--- front-end/app/view/Messages/layout.tsx
+++ front-end/app/view/Messages/layout.tsx
@@ -3,6 +3,7 @@
 import ChatSideBar from "@/components/chats/ChatSideBar";
 import { usePathname } from "next/navigation";
 import { NewChat } from "@/components/chats/messages";
+import ProtectedRoute from "@/components/protected/protected";
 
 
 export default function ChatLayout({
@@ -14,17 +15,21 @@ export default function ChatLayout({
 
   if (path === "/view/Messages") {
     return (
-    <div className="flex gap-3">
-      <ChatSideBar />
-      <NewChat />
-    </div>
-  );
+      <div className="flex gap-3">
+        <ProtectedRoute>
+          <ChatSideBar />
+          <NewChat />
+        </ProtectedRoute>
+      </div>
+    );
   }
 
   return (
     <div className="flex gap-3">
-      <ChatSideBar />
-      {children}
+      <ProtectedRoute>
+        <ChatSideBar />
+        {children}
+      </ProtectedRoute>
     </div>
   );
 }
