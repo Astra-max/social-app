0965a9a2b17d33f89ab15a4822b049b200bbccf9
Astra-max <waoremaxwel17@gmail.com>
2026-06-24 05:26:22 +0300

    feat: created a logout ui

--- front-end/components/sidebar/sidebar.tsx
+++ front-end/components/sidebar/sidebar.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import { sidebarNav } from "@/data";
+import { logOutUser } from "@/store/features/authSlice";
+import { useAppDispatch } from "@/store/hooks";
 import { RootState } from "@/store/store";
 import "@/styles/nav-side-bar.css";
 import { activeRoute, nonActive, navSideBar } from "@/styles/style";
+import { LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -12,6 +16,16 @@ import { useSelector } from "react-redux";
 export default function NavSideBar() {
   const path = usePathname();
   const { toggle } = useSelector((state: RootState) => state.toggleSideBar);
+  const [err, setErr] = useState(null)
+  const dispatch = useAppDispatch()
+
+  async function handleLogout() {
+    try {
+      await dispatch(logOutUser()).unwrap()
+    } catch (error: any) {
+      setErr(error)
+    }
+  }
 
   const newStyle = {
     ...navSideBar,
@@ -45,6 +59,14 @@ export default function NavSideBar() {
             </Link>
           );
         })}
+        <div className="cursor-pointer" onClick={handleLogout}>
+          <span className="flex items-center gap-3">
+            <LogOut 
+            className="text-red-600"
+            size={20} />
+            Logout
+          </span>
+        </div>
       </div>
     </div>
   );
